b6bb7ce6612c9b43b72020860d14e6b5fe19ad6c
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-06-30 11:04:18 +1000

    Create node server.

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Server running ...");
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is listening op port ${port}...`);
+});
